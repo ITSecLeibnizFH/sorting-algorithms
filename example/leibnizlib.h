f977d702cbfea4d029621d7f4ea41e8158dcf0f1
js-on <55274241+js-on@users.noreply.github.com>
2019-11-22 19:15:55 +0100

    added lib and template (#2)
    Squash-merged #2 on behalf of @js-on.

---
+++ example/leibnizlib.h
@@ -0,0 +1,33 @@
+/* Author:      Jakob Schaffarczyk
+ * Date:        22. Nov. 2019
+ * Description: Library containing function,
+ *              to read LeibnizFH dataset
+ *              for benchmarking the sorting
+ *              algorithms written in C.
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* LIMIT stores the length of the dataset
+ * array stores an array:
+ *     char arr[10] = {0};
+ */
+
+void readDataset(signed int *array, int LIMIT) {
+    char fname[32];
+    snprintf(fname, sizeof fname, "%s%d%s", "random_", LIMIT, ".txt");
+
+    FILE *fp;
+    fp = fopen(fname, "r");
+
+    if (fp == NULL) {
+        printf("Error reading file...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < LIMIT; i++) {
+        fscanf(fp, "%d", &array[i]);
+    }
+}
