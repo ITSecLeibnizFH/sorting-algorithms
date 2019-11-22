5041e91476dec46752cf7b8d69e07970a41f4e3e
Jakob S <jaschaffarczyk@online.de>
2019-11-22 15:52:54 +0100

    added lib and template

---
+++ example/template.c
@@ -0,0 +1,21 @@
+/* Title:       <title>
+ * Author:      <author>
+ * Date:        <date>
+ * Description: <description>
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <leibnizlib.h>
+
+// Dataset Length
+#define LIMIT 0 
+
+int main() {
+    signed int array[LIMIT] = {0};
+    readDataset(array, LIMIT);
+
+    // Algorithm starts here
+
+    // Algorithm ends here
+}
