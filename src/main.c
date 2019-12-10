9e052924d1771253fdbc8cb65bf919039d83d932
Aaron Korte <github@aaronkorte.xyz>
2019-12-10 11:19:57 +0100

    Streamlining
    Sorting is now performed on all datasets.
    Shortened logging.

--- src/main.c
+++ src/main.c
@@ -6,15 +6,6 @@
 #include "algorithms/reference/reference.h"
 #include "util/leibnizlib.h"
 
-void print_state(const unsigned int length, const int* data) {
-    if(is_sorted(length, data)) {
-        printf("Data array is in order.\n");
-    }
-    else {
-        printf("Data array is NOT in order.\n");
-    }
-}
-
 void print_duration(double start, double stop) {
     double diff = stop - start;
     printf("Execution took %.4lf seconds.\n", diff);
@@ -22,8 +13,6 @@ void print_duration(double start, double stop) {
 
 int run_algorithm(const struct sorting_algorithm alg, const unsigned int length, int* data) {
     printf("Running algorithm: %s\n", alg.a_name);
-    printf(alg.a_desc);
-    printf("\n");
 
     int bytes = sizeof(int) * length;
     int* data_lcopy = (int*) malloc(bytes);
@@ -37,13 +26,15 @@ int run_algorithm(const struct sorting_algorithm alg, const unsigned int length,
     (alg.sort_func)(length, data_lcopy);
     double stop_time = get_time();
 
-    print_state(length, data_lcopy);
-    // for (int i = 0; i < length; i++) {
-    //     printf("%d\n", data_lcopy[i]);
-    // }
+    if(!is_sorted(length, data_lcopy)) {
+        printf("Data array is NOT in order!\n");
+        return EXIT_FAILURE;
+    }
     print_duration(start_time, stop_time);
+    printf("\n");
+
     free(data_lcopy);
-    return 1;
+    return EXIT_SUCCESS;
 }
 
 int main() {    
@@ -51,22 +42,35 @@ int main() {
     printf("Sorting algorithms - speed comparison\n");
     printf("\n");
 
-    const int length = 10000;
-    printf("Benchmarking with a test dataset of size: %d\n", length);
+    const unsigned int sizes[] = {
+        1000,
+        10000,
+        100000,
+        0 // How do I terminate this correctly?
+    };
 
-    int* data = (int*) malloc(sizeof(int) * length);
-    if(data == NULL) {
-        printf("Unable to allocate memory!\n");
-        return EXIT_FAILURE;
+    unsigned int* arrPtr = sizes;
+    while(*arrPtr)
+    {
+        printf("------------------------------------------\n");
+        printf("Benchmarking with a test dataset of size: %d\n\n", *arrPtr);
+
+        int* data = (int*) malloc(sizeof(int) * *arrPtr);
+        if(data == NULL) {
+            printf("Unable to allocate memory!\n");
+            return EXIT_FAILURE;
+        }
+
+        if(readDataset(data, *arrPtr) != EXIT_SUCCESS) {
+            printf("No dataset specified, terminating..\n");
+            exit(EXIT_FAILURE);
+        }
+
+        run_algorithm(REFERENCE, *arrPtr, data);
+        free(data);
+
+        arrPtr++;
     }
     
-    if(readDataset(data, length) != EXIT_SUCCESS) {
-        printf("No dataset specified, terminating..\n");
-        exit(EXIT_FAILURE);
-    }
-
-    run_algorithm(REFERENCE, length, data);
-    free(data);
-
     return EXIT_SUCCESS;
 }
